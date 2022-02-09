f50ea7e706d148ff715802692226f22eb1919fa3
hanif9050 <hanif.ctg9050@gmail.com>
2022-02-10 05:04:36 +0600

    button work on rgb

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
+// selector
 const bg = document.querySelector(".bg-color");
+
+const btnChange = document.querySelector(".colorCode");
+
 // random number generator
 const randomColor = function () {
   const random = Math.floor(Math.random() * 255);
   return random;
 };
-bg.style.backgroundColor = `rgb(${randomColor()},${randomColor()},${randomColor()})`;
-
+// change background on click
+btnChange.addEventListener("click", () => {
+  bg.style.backgroundColor = `rgb(${randomColor()},${randomColor()},${randomColor()})`;
+});
+//
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+hexGenerator = () => {
+  return Math.floor(Math.random() * (hex.length - 1));
+};
 const result = randomColor();
-console.log(randomColor(), randomColor(), randomColor());
+console.log(hex[hexGenerator()]);
