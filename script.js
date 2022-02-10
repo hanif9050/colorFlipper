8755f499eb5f0d8481ddaaaf50787c4162b3c08a
hanif9050 <hanif.ctg9050@gmail.com>
2022-02-10 22:52:20 +0600

    rgb complete & css modify

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // selector
 const bg = document.querySelector(".bg-color");
-
+const colorName = document.querySelector(".colorName");
 const btnChange = document.querySelector(".colorCode");
 
 // random number generator
@@ -10,7 +10,9 @@ const randomColor = function () {
 };
 // change background on click
 btnChange.addEventListener("click", () => {
-  bg.style.backgroundColor = `rgb(${randomColor()},${randomColor()},${randomColor()})`;
+  const rgb = `rgb(${randomColor()},${randomColor()},${randomColor()})`;
+  bg.style.backgroundColor = rgb;
+  colorName.textContent = rgb;
 });
 //
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
